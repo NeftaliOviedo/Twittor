8dd17aac43e28e3bc6af09f33e3e4df13f5edf8f
NefOv <benjaminoviedo52@gmail.com>
2020-08-12 17:34:44 -0400

    First Commit

---
+++ js/sw-utils.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+function actualizaCacheDinamico( dynamicCache, req,resp) {
+
+
+    if(resp.ok){
+      return  caches.open(dynamicCache).then(cache => {
+
+            cache.put(req, resp.clone());
+
+            return resp.clone();
+        })
+    }else{
+        return resp;
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
